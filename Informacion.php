6e1f0cdca8b0ee1880fbc6b865835fdef0aafcfc
Javier-05 <ajavier050410@gmail.com>
2025-05-14 10:18:01 -0500

    Formulario y otros cambios
    a

--- Informacion.php
+++ Informacion.php
@@ -56,24 +56,24 @@
                     <ul class="navbar-nav  ">
 
 
-                        <li class="nav-item active">
-                            <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
-                        </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Inicio<span class="sr-only">(current)</span></a>
+              </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.php">About</a>
-                        </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">Sobre nosotros</a>
+              </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="product.php">Products</a>
-                        </li>
+              <li class="nav-item">
+                <a class="nav-link" href="product.php">Productos</a>
+              </li>
 
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="testimonial.php">Testimonial</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="contact.php">Contact us</a>
+              <li class="nav-item">
+                <a class="nav-link" href="testimonial.php">Comentarios</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contactanos</a>
 
                         </li>
                     </ul>
